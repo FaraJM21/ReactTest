19bae3682eee8b36692b2fbec2e45e6bbdf2bd67
FaraJM21 <farruxrihsiyev@gmail.com>
2023-07-13 14:27:42 +0300

    ant design / material ui added

--- src/App.js
+++ src/App.js
@@ -1,22 +1,20 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import "./assets/App.scss";
-import Character from "./pages/character";
-import Episode from "./pages/episode";
-import Home from "./pages/home";
-import Layout from "./pages/layout";
+import Character from "./pages/character/character";
+import Episode from "./pages/episode/episode";
+import Home from "./pages/home/home";
+import Layout from "./pages/layout/layout";
 function App() {
   return (
     <div className="app">
-      <div className="container">
-        <Routes>
-          <Route path="/" element={<Layout />}>
-            <Route index element={<Home />} />
-            <Route path="character" element={<Character />} />
-            <Route path="episode" element={<Episode />} />
-          </Route>
-        </Routes>
-      </div>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="character" element={<Character />} />
+          <Route path="episode" element={<Episode />} />
+        </Route>
+      </Routes>
     </div>
   );
 }
---
+++ src/pages/index.js
@@ -0,0 +1,4 @@
+export { default as Home } from "./home/home";
+export { default as Character } from "./character/character";
+export { default as Episode } from "./episode/episode";
+export { default as Layout } from "./layout/layout";
