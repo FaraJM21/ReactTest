0580826c9595fa645a170a23c8a9c27006cb1522
FaraJM21 <farruxrihsiyev@gmail.com>
2023-07-16 20:01:34 +0300

    problem soluted

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -2,7 +2,7 @@ import { Container } from "@mui/system";
 import React from "react";
 import { Link } from "react-router-dom";
 import logo from "../../assets/logo.png";
-import SwitchTheme from "../switch/switchTheme";
+
 
 import "./style.scss";
 function Navbar() {
@@ -15,7 +15,7 @@ function Navbar() {
               <img src={logo} alt="404" />
             </Link>
           </div>
-          <SwitchTheme />
+        
         </div>
       </Container>
     </header>
